d5058ac1f6d4a5d7be2f09fcf5530cc5425b82a5
ObsidianSeal <bmgosselin@gmail.com>
2026-05-02 01:58:25 -0400

    small improvements to hopefully remove undefined redirect

--- public/assets/js/links.js
+++ public/assets/js/links.js
@@ -1,31 +1,37 @@
 "use strict";
 
 const defaultURL = "https://linktr.ee/uwite";
-if (window.location.href == "https://ite.fyi/") window.location.href = defaultURL;
-
 let redirected = false;
-try {
-	const requestURL = "https://ite.fyi/assets/csv/links.csv";
-	const request = new Request(requestURL);
-	const response = await fetch(request);
-	const text = await response.text();
 
-	let links = text.split("\n");
-	let header = true;
-	for (let link of links) {
-		if (header) {
-			header = false;
-			continue;
+if (window.location.href == "https://ite.fyi/") {
+	redirected = true;
+	window.location.href = defaultURL;
+}
+
+if (!redirected) {
+	try {
+		const requestURL = "https://ite.fyi/assets/csv/links.csv";
+		const request = new Request(requestURL);
+		const response = await fetch(request);
+		const text = await response.text();
+
+		let links = text.split("\n").filter((line) => line.trim() != "");
+		let header = true;
+		for (let link of links) {
+			if (header) {
+				header = false;
+				continue;
+			}
+			let from = link.split(",")[0];
+			let to = link.split(",")[1];
+			if (window.location.pathname.substring(1) == from) {
+				redirected = true;
+				window.location.href = to;
+			}
 		}
-		let from = link.split(",")[0];
-		let to = link.split(",")[1];
-		if (window.location.href.substring(16) == from) {
-			redirected = true;
-			window.location.href = to;
-		}
+	} catch (error) {
+		console.error(error);
 	}
-} catch (error) {
-	console.error(error);
 }
 
 if (!redirected) window.location.href = defaultURL;
